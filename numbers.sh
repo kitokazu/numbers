b0b198a2e58201a11c63895b8e529cc033a1896c
Kai Itokazu <itokazu@chapman.edu>
2019-11-19 15:27:13 -0800

    Add numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers.sh
+# Kai Itokazu
+echo "Enter a positive number: "
+read N
+count=1
+while [ $count -le $N ]
+do
+	if [ $((count%2)) -eq 0 ]
+	then
+		echo "$count Even"
+	else
+		echo "$count Odd"
+	fi
+	count=$((count+1))
+done
+
